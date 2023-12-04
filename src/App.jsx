2b0093901b9b74f8f363b41fea9ccbdf37d05bda
dani <danifromecuador@gmail.com>
2023-12-03 21:18:43 -0500

    Create test fetch on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import './App.css'
 
+function fetchData() {
+  fetch('http://127.0.0.1:3000/health')
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+
 function App() {
+  fetchData()  
+
   return (
     <div className="App">
       <h1>React App</h1>
